16590290db45e56688cd5e8385587e4053f3c54f
Anatoliy Komarov <admin@komaroff.biz>
2021-06-07 08:24:36 +0300

    Update product-form.js

--- public/js/product-form.js
+++ public/js/product-form.js
@@ -1,10 +1,11 @@
+let numberOfForm=2;// amount of forms on the page ()
 let formProductPattern = `
-<form class="products-form" id="form2">
-                    <p class="h2 row-between2">Product #2 <span class="del_product" id="product2">x</span></p>
-                    <label class="h3" for="cartons2">1) No. OF Cartons</label><br>
-                    <input class="request-input-text" type="text" value="" name="" id="cartons2" placeholder="01"
-                        list="carton2_list">
-                    <datalist id="carton2_list">
+<form class="products-form" id="form${numberOfForm}">
+                    <p class="h2 row-between2">Product #${numberOfForm} <span class="del_product" id="product${numberOfForm}">x</span></p>
+                    <label class="h3" for="cartons${numberOfForm}">1) No. OF Cartons</label><br>
+                    <input class="request-input-text" type="text" value="" name="" id="cartons${numberOfForm}" placeholder="01"
+                        list="carton${numberOfForm}_list">
+                    <datalist id="carton${numberOfForm}_list">
                         <option value="01"></option>
                         <option value="02"></option>
                         <option value="03"></option>
@@ -14,25 +15,25 @@ let formProductPattern = `
                         <option value="07"></option>
                     </datalist>
                     <p class="h3">2)
-                        <input class="radio" type="radio" name="size_carton2" id="size1-2">
-                        <label for="size1-2">Total Volume</label>
-                        <input class="radio" type="radio" name="size_carton2" id="size2-2" checked>
-                        <label for="size2-2">Single Carton Dimensions</label>
-                        <input class="request-little-input-text" type="text" name="" id="s1-2" value="">
-                        <label for="s1-2"> CM</label>
-                        <input class="request-little-input-text" type="text" name="" id="s2-2" value="">
-                        <label for="s2-2"> CM</label>
-                        <input class="request-little-input-text" type="text" name="" id="s3-2" value="">
-                        <label for="s3-2"> CM</label>
+                        <input class="radio" type="radio" name="size_carton${numberOfForm}" id="size1-${numberOfForm}">
+                        <label for="size1-${numberOfForm}">Total Volume</label>
+                        <input class="radio" type="radio" name="size_carton${numberOfForm}" id="size2-${numberOfForm}" checked>
+                        <label for="size2-${numberOfForm}">Single Carton Dimensions</label>
+                        <input class="request-little-input-text" type="text" name="" id="s1-${numberOfForm}" value="">
+                        <label for="s1-${numberOfForm}"> CM</label>
+                        <input class="request-little-input-text" type="text" name="" id="s2-${numberOfForm}" value="">
+                        <label for="s2-${numberOfForm}"> CM</label>
+                        <input class="request-little-input-text" type="text" name="" id="s3-${numberOfForm}" value="">
+                        <label for="s3-${numberOfForm}"> CM</label>
                     </p>
                     <p class="h3">3)
-                        <input class="radio" type="radio" name="total_weight2" id="carton_weight2">
-                        <label for="carton_weight2">Carton weight</label>
-                        <input class="radio" type="radio" name="total_weight2" id="total_weight2" checked>
-                        <label for="total_weight2">Total weight</label>
+                        <input class="radio" type="radio" name="carton_weight${numberOfForm}" value="carton_weight${numberOfForm}" id="carton_weight${numberOfForm}">
+                        <label for="carton_weight${numberOfForm}">Carton weight</label>
+                        <input class="radio" type="radio" name="total_weight${numberOfForm}"  value="total_weight${numberOfForm}" id="total_weight${numberOfForm}" checked>
+                        <label for="total_weight${numberOfForm}">Total weight</label>
                         <input class="request-little-input-text" style="width: 150px;margin-left:114px;" type="text"
-                            name="" id="tw2" value="">
-                        <label for="tw2"> KG</label>
+                            name="" id="tw${numberOfForm}" value="">
+                        <label for="tw${numberOfForm}"> KG</label>
                     </p>
                 </form>
 `;
