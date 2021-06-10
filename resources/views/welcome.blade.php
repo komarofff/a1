894bc75a5500f7f99f124b5ba2d93825c9438f03
Anatoliy Komarov <admin@komaroff.biz>
2021-06-10 10:22:14 +0300

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,26 +51,26 @@ $('.main__testimonials-slider').slick({
             
             <div class="header__block-list">
                 <p class="header__block-list-link">
-                    <img class="header__block-list-image" src="{{asset('/img/header/layer-24.png')}}"
-                        srcset="{{asset('/img/header/layer-24@2x.png')}}">
+                    <img class="header__block-list-image" src="{{asset('/img/svg/layer-24.svg')}}"
+                        srcset="{{asset('/img/svg/layer-24@2x.svg')}}">
                     <span class="h3">Shipping</span>
                     <span class="text">An hoc usque quaque</span>
                 </p><img class="head_block_stripe" src="{{asset('/img/header/layer-38.png')}}">
                 <p class="header__block-list-link">
-                    <img class="header__block-list-image" src="{{asset('/img/header/layer-28.png')}}"
-                        srcset="{{asset('/img/header/layer-28@2x.png')}}">
+                    <img class="header__block-list-image" src="{{asset('/img/svg/layer-28.svg')}}"
+                        srcset="{{asset('/img/svg/layer-28@2x.svg')}}">
                     <span class="h3">Tracking</span>
                     <span class="text">An hoc usque quaque</span>
                 </p><img class="head_block_stripe" src="{{asset('/img/header/layer-38.png')}}">
                 <p class="header__block-list-link">
-                    <img class="header__block-list-image" src="{{asset('/img/header/layer-37.png')}}"
-                        srcset="{{asset('/img/header/layer-37@2x.png')}}">
+                    <img class="header__block-list-image" src="{{asset('/img/svg/layer-37.svg')}}"
+                        srcset="{{asset('/img/svg/layer-37@2x.svg')}}">
                     <span class="h3">Train</span>
                     <span class="text">An hoc usque quaque</span>
                 </p><img class="head_block_stripe" src="{{asset('/img/header/layer-38.png')}}">
                 <p class="header__block-list-link">
-                    <img class="header__block-list-image" src="{{asset('/img/header/layer-29.png')}}"
-                        srcset="{{asset('/img/header/layer-29@2x.png')}}">
+                    <img class="header__block-list-image" src="{{asset('/img/svg/layer-29.svg')}}"
+                        srcset="{{asset('/img/svg/layer-29@2x.svg')}}">
                     <span class="h3">Plane</span>
                     <span class="text">An hoc usque quaque</span>
                 </p>
